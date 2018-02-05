9df23a977d4fb3d6b6ba4df5a293c8fdb6914bb1
Tiago Pinto <7tiago.pinto@gmail.com>
2018-02-05 13:36:25 +0000

    Task Manager is now able to store missions in a pile a execute them when the previous mission is completed. Issue #52

--- task_manager_server/src/task_manager_class.py
+++ task_manager_server/src/task_manager_class.py
@@ -22,6 +22,7 @@ class TaskManager(object):
         self.skills = skills if skills is not None else []
         self.ongoingTasks = []
         self.missions = []
+        self.mission_execution_pile = []
 
         self.waitForServerTimeOut = waitForServerTimeOut if waitForServerTimeOut is not None else 10
         self.waitForActionClientTimeOut = waitForActionClientTimeOut if waitForActionClientTimeOut is not None else 300
@@ -64,7 +65,6 @@ class TaskManager(object):
 
     ## NOTE: ROS specific
     def assign_mission_service_handler(self, missionId = 'defaultMissionId', robotId = 'defaultRobotId', goals = [], executeMission = False):
-        tasksAux = []
 
         if robotId != self.robotId:
             rospy.logwarn('[TaskManager] [' + str(self.robotId) + '] Mission ' + str(missionId) + ' refused: Different robotId!')
@@ -76,36 +76,58 @@ class TaskManager(object):
                 rospy.logwarn('[TaskManager] [' + str(self.robotId) + '] Mission ' + str(missionId) + ' refused: Mission already stored!')
                 return MSGConstructor.ActionAcceptedRefusedConstructor(accepted='False', reasonOfRefusal='Mission already stored!')
 
+        # if len(self.ongoingTasks) != 0:
+        #     rospy.logwarn('[TaskManager] [' + str(self.robotId) + '] Mission ' + str(missionId) + ' refused: Robot is currently busy with ' + str(len(self.ongoingTasks)) + ' tasks: ' + str([t.skillName for t in self.ongoingTasks]))
+        #     return MSGConstructor.ActionAcceptedRefusedConstructor(accepted = 'False', reasonOfRefusal = 'Robot is currently busy with ' + str(len(self.ongoingTasks)) + ' tasks: ' + str([t.skillName for t in self.ongoingTasks]))
 
-        if len(self.ongoingTasks) != 0:
-            rospy.logwarn('[TaskManager] [' + str(self.robotId) + '] Mission ' + str(missionId) + ' refused: Robot is currently busy with ' + str(len(self.ongoingTasks)) + ' tasks: ' + str([t.skillName for t in self.ongoingTasks]))
-            return MSGConstructor.ActionAcceptedRefusedConstructor(accepted = 'False', reasonOfRefusal = 'Robot is currently busy with ' + str(len(self.ongoingTasks)) + ' tasks: ' + str([t.skillName for t in self.ongoingTasks]))
-
-        elif len(goals) == 0:
+        if len(goals) == 0:
             rospy.logwarn('[TaskManager] [' + str(self.robotId) + '] Mission ' + str(missionId) + ' refused: Empty goals!')
             return MSGConstructor.ActionAcceptedRefusedConstructor(accepted = 'False', reasonOfRefusal = 'Empty goals!')
 
+        for goal in goals:
+            try:
+                SkillFactory(goal, self.skills)
+            except Exception as e:
+                rospy.logwarn('[TaskManager] [' + str(self.robotId) + '] Mission ' + str(missionId) + ' refused: Badly formatted goal (' + str(goal) + '): ' + str(e))
+                return MSGConstructor.ActionAcceptedRefusedConstructor(accepted = 'False', reasonOfRefusal = 'Badly formatted goal (' + str(goal) + '): ' + str(e))
+
         else:
-            for goal in goals:
-                try:
-                    sf = SkillFactory(goal, self.skills)
-                    tasksAux += [sf.skill]
-                except Exception as e:
-                    rospy.logwarn('[TaskManager] [' + str(self.robotId) + '] Mission ' + str(missionId) + ' refused: Badly formatted goal (' + str(goal) + '): ' + str(e))
-                    return MSGConstructor.ActionAcceptedRefusedConstructor(accepted = 'False', reasonOfRefusal = 'Badly formatted goal (' + str(goal) + '): ' + str(e))
+            self.missions.append({'missionId': missionId, 'taskId': '', 'statusCode':'', 'statusDescription': 'Mission Created'})
+            mission_to_pile = {'missionId': missionId, 'robotId': robotId, 'goals': goals, 'executeMission': executeMission}
+            self.mission_execution_pile.append(mission_to_pile)
+            if len(self.ongoingTasks) != 0:
+                rospy.loginfo('[TaskManager] [' + str(self.robotId) + '] Mission ' + str(missionId) + ' Accepted! Waiting for execution.')
+                return MSGConstructor.ActionAcceptedRefusedConstructor(accepted = 'True', reasonOfRefusal = 'None')
+            else:
+                rospy.loginfo('[TaskManager] [' + str(self.robotId) + '] Mission ' + str(missionId) + ' Accepted! Starting Execution.')
+                self.pile_execution_handler()
+                return MSGConstructor.ActionAcceptedRefusedConstructor(accepted = 'True', reasonOfRefusal = 'None')
 
-            # If mission is accepted...
-            self.ongoingTasks = tasksAux
 
-            rospy.loginfo('[TaskManager] [' + str(self.robotId) + '] Mission ' + str(missionId) + ' Accepted!')
+    def pile_execution_handler(self):
 
-            if executeMission:
-                # TODO: Make sure that this is the appropriate way of executing a thread in Python
-                # IDEA: Maybe the Thread object attribute 'Name' can be used to pause / stop an ongoing thread execution
-                # TODO: IMPORTANT! CRITICAL! Do not forget to handle mutex / semaphore for accessing self.ongoingTasks!!!
-                threading.Thread(target=self.execute_mission, args=(missionId,)).start()
+        if not self.mission_execution_pile:
+            rospy.loginfo('[TaskManager] [' + str(self.robotId) + '] There are no more missions to execute.')
+            return
 
-            return MSGConstructor.ActionAcceptedRefusedConstructor(accepted = 'True', reasonOfRefusal = 'None')
+        missionId = self.mission_execution_pile[0]['missionId']
+        robotId = self.mission_execution_pile[0]['robotId']
+        goals = self.mission_execution_pile[0]['goals']
+        executeMission = self.mission_execution_pile[0]['executeMission']
+
+        tasksAux = []
+
+        for goal in goals:
+            sf = SkillFactory(goal, self.skills)
+            tasksAux += [sf.skill]
+        self.ongoingTasks = tasksAux
+
+        if executeMission:
+            # TODO: Make sure that this is the appropriate way of executing a thread in Python
+            # IDEA: Maybe the Thread object attribute 'Name' can be used to pause / stop an ongoing thread execution
+            # TODO: IMPORTANT! CRITICAL! Do not forget to handle mutex / semaphore for accessing self.ongoingTasks!!!
+            threading.Thread(target=self.execute_mission, args=(missionId,)).start()
+        return
 
     # TODO: Unit Test
     def update_mission_status(self, missionId, taskId, statusCode, statusDescription):
@@ -127,9 +149,9 @@ class TaskManager(object):
 
     ## NOTE: ROS specific
     def execute_mission(self, missionId):
-        self.missions.append({'missionId': missionId, 'taskId': '', 'statusCode':'', 'statusDescription': 'Mission Created'})
+        #self.missions.append({'missionId': missionId, 'taskId': '', 'statusCode':'', 'statusDescription': 'Mission Created'})
 
-        rospy.loginfo('[TaskManager] [' + str(self.robotId) + '] Starting Mission with ' + str(len(self.ongoingTasks)) + ' Tasks... ')
+        rospy.loginfo('[TaskManager] [' + str(self.robotId) + '] Starting Mission ' + str(missionId) + ' with ' + str(len(self.ongoingTasks)) + ' Tasks... ')
 
         while len(self.ongoingTasks) is not 0:
             task = self.ongoingTasks.pop(0) # TODO: Write a Unit Test for this!
@@ -156,9 +178,9 @@ class TaskManager(object):
 
                 self.update_mission_status(missionId = missionId, taskId = task.skillName, statusCode = 4, statusDescription = 'Task ' + str(task.skillName) + ' Failed: ' + str(taskStatus))
                 self.update_mission_status(missionId = missionId, taskId = task.skillName, statusCode = 12, statusDescription = 'Mission Failed! Task ' + str(task.skillName) + ' Failed: ' + str(taskStatus))
-
+                self.mission_execution_pile = self.mission_execution_pile[1:]
                 return False
 
         self.update_mission_status(missionId = missionId, taskId = task.skillName, statusCode = 11, statusDescription = 'Mission Success!')
-
-        return True
+        self.mission_execution_pile = self.mission_execution_pile[1:]
+        return self.pile_execution_handler()
